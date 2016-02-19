b8bff7de7cc6927d58b4ab45cf74a87361329a49
kk247 <kk247@2446c6b1-c689-4864-9835-4c814a457dcf>
2016-02-19 20:48:06 +0000

    sqLite database created which will store bought items items are recorded before payment array returned to adapter modified - boolean to record if the element has been found in the database created. If the element has been bought before the item will be marked as predicted

---
+++ code/branches/androidApp/smartShopping/src/com/example/adapters/groceryAdapter.java
@@ -0,0 +1,52 @@
+package com.example.adapters;
+
+import java.util.ArrayList;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import classes.Shops;
+import classes.groceryItem;
+
+import com.example.smartshopping.*;
+
+public class groceryAdapter extends ArrayAdapter<groceryItem>{
+
+
+	private Context context;
+
+	public groceryAdapter(Context context, int textViewResourceId,
+			ArrayList<groceryItem> dataObject) {
+		super(context, textViewResourceId, dataObject);
+		this.context=context;
+		
+	}
+
+
+	// create the view
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView=convertView;
+		if(rowView==null){
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
+		rowView = inflater.inflate(R.layout.grocery_rows, parent, false);
+		
+		}
+		
+		TextView textView = (TextView) rowView.findViewById(R.id.name);
+		TextView textView1 = (TextView) rowView.findViewById(R.id.price);
+		// gets the name and the price of the item at the given position 
+		textView.setText(""+getItem(position).name);
+		
+		textView1.setText(""+"£"+getItem(position).Price+" per "+getItem(position).per+getItem(position).suggested);
+		return rowView;
+	}
+
+	
+
+	
+	}
